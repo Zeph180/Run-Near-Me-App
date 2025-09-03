59265d2c7b150e4596be872b731b27cffb597a37
zeph180 <zephrichards1@gmail.com>
2025-09-03 19:50:04 +0300

    Create form structural component

---
+++ src/components/FormInput.tsx
@@ -0,0 +1,29 @@
+﻿import React from "react";
+import {Text, TextInput, View} from "react-native";
+
+
+const FormInput = (
+    label: string,
+    value: string,
+    onChangeText: (text: string) => void,
+    placeholder: string,
+    secureTextEntry: boolean,
+    autoCapitalize: "none",
+    keyboardType: "default"
+) => {
+    return (
+        <View>
+            <Text>{label}</Text>
+            <TextInput 
+                value={value}
+                onChangeText={onChangeText}
+                placeholder={placeholder}
+                secureTextEntry={secureTextEntry}
+                autoCapitalize={autoCapitalize}
+                keyboardType={keyboardType}
+            />
+        </View>
+    );
+}
+
+export default FormInput;
