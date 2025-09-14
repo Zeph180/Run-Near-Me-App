5cce9a1648e90525ba9e01626eed3d66a8146a62
zeph180 <zephrichards1@gmail.com>
2025-09-14 18:36:21 +0300

    Create custom tabs switcher

---
+++ src/components/TabSwitcher.tsx
@@ -0,0 +1,13 @@
+﻿import {useState} from "react";
+import {View} from "react-native";
+
+const TabSwitcher = () => {
+  const [activeTab, setActiveTab] = useState<boolean>(false);
+  const [indicatorAnim] = useState<boolean>(false);
+  
+  return (
+      <View>
+          
+          </View>
+  )
+};
