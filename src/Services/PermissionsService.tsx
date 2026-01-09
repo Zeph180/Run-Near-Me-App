5abe17d66a97e8eae9f3b83ec79711e90fda7a9e
zeph180 <zephrichards1@gmail.com>
2026-01-09 22:04:10 +0300

    Create sensor hook

---
+++ src/Services/PermissionsService.tsx
@@ -0,0 +1,6 @@
+﻿import * as Location from "expo-location";
+
+export async function requestPermissionsService() {
+  await Location.requestForegroundPermissionsAsync();
+  await Location.requestBackgroundPermissionsAsync();
+}
